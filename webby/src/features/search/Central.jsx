350501ae874058efc41fc459198a3c6193824f4e
Alex Rothberg <alexrothberg13@gmail.com>
2021-01-24 10:42:31 -0500

    new api key too

--- webby/src/features/search/Central.jsx
+++ webby/src/features/search/Central.jsx
@@ -9,38 +9,48 @@ class Central extends React.Component {
     this.state = {
       data: ' ',
       url: ' ',
+      imgUrl: ' ',
       title: ' ',
       query: this.props.query,
     };
   }
   componentWillReceiveProps() {
+    const key = 'AIzaSyDT5bQz1R6Sw9yTToQ5T5og9YkhYFT2oIs'
     console.log('url next');
     console.log(
-      `https://www.googleapis.com/customsearch/v1?q=${this.props.query}&key=AIzaSyCTWna4c9sM7nDNFqw8O71FymNyM27AG4A&tbm=nws&cx=792b3c9d19def1928`
+      `https://www.googleapis.com/customsearch/v1?q=${this.props.query}&key=${key}&tbm=nws&cx=792b3c9d19def1928`
     );
     get(
-      `https://www.googleapis.com/customsearch/v1?q=${this.props.query}&key=AIzaSyCTWna4c9sM7nDNFqw8O71FymNyM27AG4A&tbm=nws&cx=792b3c9d19def1928`
+      `https://www.googleapis.com/customsearch/v1?q=${this.props.query}&key=${key}&tbm=nws&cx=792b3c9d19def1928`
     ).then((res) =>
       this.setState({
         data: res.items[0].snippet,
         url: res.items[0].formattedUrl,
         title: res.items[0].title,
+        imgUrl: res.items[0].pagemap.cse_thumbnail[0].src
       })
     );
   }
 
   render() {
-    const { data, url, title } = this.state;
+    const { data, url, title,imgUrl } = this.state;
     return (
       <div className="d-flex flex-row text-wrap">
         <p className="text-wrap">
-          Center article title: {title}
+          Center article title:
+          <a href={url}>{title}</a>;
+          <br></br>
+          <img 
+           src={imgUrl}
+           alt="photo"
+           />
           <br></br>
           <br></br>
           Center article snippet: {data}
           <br></br>
           <br></br>
           URL: {url}
+  
         </p>
       </div>
     );
--- webby/src/features/search/Libs.jsx
+++ webby/src/features/search/Libs.jsx
@@ -10,37 +10,49 @@ class Libs extends React.Component {
       data: ' ',
       url: ' ',
       title: ' ',
+      imgUrl: ' ',
       query: this.props.query,
     };
   }
   componentWillReceiveProps() {
+    const key = 'AIzaSyDT5bQz1R6Sw9yTToQ5T5og9YkhYFT2oIs'
+
     console.log('url next');
     console.log(
-      `https://www.googleapis.com/customsearch/v1?q=${this.props.query}&key=AIzaSyCTWna4c9sM7nDNFqw8O71FymNyM27AG4A&tbm=nws&cx=b2429b4ec66f52cb6`
+      `https://www.googleapis.com/customsearch/v1?q=${this.props.query}&key=${key}&tbm=nws&cx=b2429b4ec66f52cb6`
     );
     get(
-      `https://www.googleapis.com/customsearch/v1?q=${this.props.query}&key=AIzaSyCTWna4c9sM7nDNFqw8O71FymNyM27AG4A&tbm=nws&cx=b2429b4ec66f52cb6`
+      `https://www.googleapis.com/customsearch/v1?q=${this.props.query}&key=${key}&tbm=nws&cx=b2429b4ec66f52cb6`
     ).then((res) =>
       this.setState({
         data: res.items[0].snippet,
         url: res.items[0].formattedUrl,
         title: res.items[0].title,
+        imgUrl: res.items[0].pagemap.cse_thumbnail[0].src
       })
     );
   }
 
   render() {
-    const { data, url, title } = this.state;
+    const { data, url, title,imgUrl } = this.state;
     return (
       <div className="d-flex flex-row text-wrap">
         <p className="text-wrap">
-          Liberal article title: {title}
+          Liberal article title: 
+          <a href={url}>{title}</a>;
+          
+          <br></br>
+          <img 
+          src={imgUrl}
+          alt="Photo"
+          />
           <br></br>
           <br></br>
           Liberal article snippet: {data}
           <br></br>
           <br></br>
           URL: {url}
+
         </p>
       </div>
     );
